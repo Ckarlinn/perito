f81dd7f99c2c7220decbca43d93a4f25dc796b84
Ckarlinn <munoz.ckarlos@gmail.com>
2025-09-04 11:58:04 -0600

    build: add Tailwind CSS pipeline

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./ui.html",
+    "./js/**/*.{js,ts}",
+    "./backend/**/*.{js,ts}",
+    "./docs/**/*.{html,md}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
